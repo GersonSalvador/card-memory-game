6d7b50fd33927eef4dd3963bbabfa4eed45093e0
Gerson Salvador <gerson.salvador@outlook.com>
2021-06-20 18:57:34 -0300

    Fix Resign

--- scripts/resign.mjs
+++ scripts/resign.mjs
@@ -7,13 +7,14 @@ export function resign(){
 
   const board = document.getElementById('board')
   const title = 'Shame on you!'
-  const text = 'You lost the '
+  const text = 'You lost the game'
   const type = 'failed'
   
-  handleStartBtn('resign')
+  handleStartBtn('start')
 
   board.style.display = 'none'
   board.innerHTML = ''
 
   setMessage({title, text, type})
+
 }
